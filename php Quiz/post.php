c2152de0f3f73bbd34616acbe664039ed98af053
prakash <prakashindu212@gmail.com>
2025-03-06 12:38:53 +0530

    Third Commit: Add Form Validations

---
+++ php Quiz/post.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<form method="POST" action="example.php">
+  Name: <input type="text" name="name">
+  <br>
+  E-mail: <input type="text" name="email">
+  <br>
+  password: <input type="password" name="password">
+  <br>
+  <br>
+  <input type="submit">
+</form>
+
+</body>
+</html>
